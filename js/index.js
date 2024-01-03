65b84fdf216a45b7c78f656a545abad6519d87be
Diana Islas Ocampo <dev@hyuchia.com>
2024-01-03 11:38:05 -0600

    Update base configuration

--- js/index.js
+++ js/index.js
@@ -4,10 +4,7 @@
  * TODO: Find a way to import Font Awesome in a better way
  */
 
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-
-import './../node_modules/@fortawesome/fontawesome-free/js/all.js';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 /**
  * Import needed libraries for the website
@@ -16,11 +13,11 @@ import { $_ready, Platform } from '@aegis-framework/artemis';
 
 
 // Check if the platform supports service workers and register them
-if (Platform.serviceWorkers ()) {
-	navigator.serviceWorker.register ('./../service-worker.js');
+if (Platform.serviceWorkers()) {
+	navigator.serviceWorker.register(new URL('../service-worker.js', import.meta.url), { type: 'module' });
 }
 
 // When the page is ready, you can start modifying its DOM!
-$_ready (() => {
+$_ready(() => {
 
 });
--- service-worker.js
+++ service-worker.js
@@ -1,96 +1,54 @@
-'use strict';
+import { manifest, version } from '@parcel/service-worker';
 
+const IDENTIFIER = `oregano-v${version}`;
 
-// Name of your cache, no spaces or special characters.
-var name = '';
+async function install () {
+	const cache = await caches.open(IDENTIFIER);
+	await cache.addAll(manifest);
+}
 
-// The version of the cache, changing this will force everything to be cached
-// again.
-var version = '';
+async function activate () {
+	const keys = await caches.keys();
+	const promises = keys.map(key => key !== IDENTIFIER && caches.delete(key));
+	await Promise.all(promises);
+}
 
-var files = [
-	'index.html',
+self.addEventListener('install', event => event.waitUntil(install()));
 
-	// Style Sheets
-	'style/font-awesome.min.css',
-	'style/animate.min.css',
-	'style/kayros.css',
-	'style/main.css',
-
-	// Scripts
-	'js/index.js',
-	'js/fontawesome.min.js',
-
-	// Images
-	'img/favicon.ico',
-	'img/icons/icon_48x48.png',
-	'img/icons/icon_60x60.png',
-	'img/icons/icon_70x70.png',
-	'img/icons/icon_76x76.png',
-	'img/icons/icon_96x96.png',
-	'img/icons/icon_128x128.png',
-	'img/icons/icon_150x150.png',
-	'img/icons/icon_152x152.png',
-	'img/icons/icon_167x167.png',
-	'img/icons/icon_180x180.png',
-	'img/icons/icon_192x192.png',
-	'img/icons/icon_310x310.png',
-	'img/icons/icon_512x512.png'
-];
-
-self.addEventListener('install', function (event) {
-	event.waitUntil(
-		caches.open(`${name}-v${version}`).then(function (cache) {
-			return cache.addAll(files);
-		})
-	);
-});
-
-self.addEventListener('activate', function (event) {
-	event.waitUntil(
-		caches.keys().then(function (keyList) {
-			return Promise.all(keyList.map(function (key) {
-				if (key !== `${name}-v${version}`) {
-					return caches.delete(key);
-				}
-			}));
-		})
-	);
-
+self.addEventListener('activate', event => {
+	event.waitUntil(activate());
 	return self.clients.claim();
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', event => {
+
 	if (event.request.method !== 'GET') {
 		return;
 	}
 
 	event.respondWith(
-		caches.match(event.request).then(function (cached) {
-			var networked = fetch(event.request)
-				.then(fetchedFromNetwork, unableToResolve)
-				.catch(unableToResolve);
-			return cached || networked;
+		caches.match(event.request).then(cached => {
+			const fetchedFromNetwork = async response => {
+				const cache = await caches.open(IDENTIFIER);
 
-			function fetchedFromNetwork (response) {
-				var cacheCopy = response.clone();
-
-				caches.open(`${name}-v${version}`).then(function add (cache) {
-					cache.put(event.request, cacheCopy);
-				});
+				cache.put(event.request, response.clone());
 
 				return response;
-			}
+			};
 
-			function unableToResolve () {
-				return new Response('<h1>Service Unavailable</h1>', {
-					status: 503,
-					statusText: 'Service Unavailable',
-					headers: new Headers({
-						'Content-Type': 'text/html'
-					})
-				});
-			}
+			const unableToResolve = () => new Response(`
+				<!DOCTYPE html><html lang=en><title>Bad Request</title><meta charset=UTF-8><meta content="width=device-width,initial-scale=1"name=viewport><style>body,html{width:100%;height:100%}body{text-align:center;color:#545454;margin:0;display:flex;justify-content:center;align-items:center;font-family:-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans","Fira Sans","Droid Sans","Helvetica Neue",sans-serif}h1,h2{font-weight:lighter}h1{font-size:4em}h2{font-size:2em}</style><div><h1>Service Unavailable</h1><h2>Sorry, the server is currently unavailable or under maintenance, try again later.</h2></div>
+			`, {
+				status: 503,
+				statusText: 'Service Unavailable',
+				headers: new Headers({ 'Content-Type': 'text/html' }),
+			});
+
+			const networked = fetch(event.request)
+				.then(fetchedFromNetwork, unableToResolve)
+				.catch(unableToResolve);
+
+			return cached || networked;
 		})
 	);
 });
